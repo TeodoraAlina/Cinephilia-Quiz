30dbd27cb07e5c6a03b85168f2f1bbc5431c01c9
TeodoraAlina <alina.teower@gmail.com>
2023-02-03 03:58:23 +0000

    Add code to show quiz box

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
 
 /** 
@@ -19,4 +20,14 @@ start_btn.onclick = ()=>{
  */
 exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo");
+}
+
+/**
+ * If continueQuiz button clicked
+ * hide Info Box
+* show Quiz Box
+*/
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
 }
