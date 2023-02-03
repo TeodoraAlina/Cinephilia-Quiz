055f0d7610766ee416f5e0acefc7317fa5891d50
TeodoraAlina <alina.teower@gmail.com>
2023-02-03 04:34:38 +0000

    Get options from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,12 @@ let que_count = 0;
  */
 function showQuestion(index) {
 const que_text = document.querySelector(".que_text");
+const option_list = document.querySelector(".option_list");
 let que_tag = '<span>'+ questions[index].question +'</span>';
+let option_tag = '<div class="option">' + questions[index].options[0] +'<span></span></div>'
+                 + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                 + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                 + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
 que_text.innerHTML = que_tag;
+option_list.innerHTML = option_tag;
 }
